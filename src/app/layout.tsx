7d6f0318d87882ed6fd4e4215d6fa8164ff913c0
codeWithAime <mwizerwaaime9@gmail.com>
2026-03-13 11:13:15 +0200

    Added: Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,18 @@ export const metadata: Metadata = {
       "Connect your team with the infrastructure they need to build, ship, and scale.",
     type: "website",
   },
+  icons: {
+    icon: [
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  appleWebApp: {
+    title: "bigcradle",
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
